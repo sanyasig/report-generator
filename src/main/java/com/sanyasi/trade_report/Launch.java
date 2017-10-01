7fd062fcf28b1d9e59e433c0be9b45c7e1d9722f
sanyasi <nani76.gsrao@gmail.com>
2017-10-01 16:41:17 +0100

    updated gradle tasks

--- src/main/java/com/sanyasi/trade_report/Launch.java
+++ src/main/java/com/sanyasi/trade_report/Launch.java
@@ -15,6 +15,11 @@ public class Launch {
 	private static final Logger log = LoggerFactory.getLogger(CsvLoader.class);
 
 	public static void main(String[] args) throws TradeReportFailedException, RecordParseException {
+		if(args.length == 0) {
+			log.error("Please provice a trades CSv file /n Sample Usage ***  java -jar report-generator-fatJar-1.0.jar PATH_TO_CSV   ***  ");
+			System.exit(0);
+		}
+		
 		String filePath = args[0];
 		
 		if(!new File(filePath).exists())
